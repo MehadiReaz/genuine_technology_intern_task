b50a18b70fa96ff6926d0e7a767bfd89dbac9977
MehadiReaz <reazmehadihasan@gmail.com>
2024-02-23 01:22:50 +0600

    dsds

---
+++ android/app/src/main/kotlin/com/example/future_it_task/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.future_it_task
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'app/core/app.dart';
+
+void main() {
+  runApp(const App());
+}
